3ae2e13f181273caeb99d17258c29d0c2f0ff49b
André Gabriel <andreon098@gmail.com>
2025-04-20 11:51:29 -0300

    feat(sidebar): implement app sidebar component
    - Adds new AppSidebar component with navigation items
    - Updates sidebar UI components with new icons and styling
    - Refactors sidebar trigger to use chevron icons

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,62 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { Banknote, Home, Landmark } from 'lucide-react';
+import { Separator } from './ui/separator';
+import { Link } from '@tanstack/react-router';
+
+const items = [
+  {
+    title: 'Home',
+    url: '/',
+    icon: Home,
+  },
+  {
+    title: 'Instituições',
+    url: '/institutions',
+    icon: Landmark,
+  },
+  {
+    title: 'Doações',
+    url: '/donations',
+    icon: Banknote,
+  },
+] as const;
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <h1 className="text-2xl font-bold">Reapp</h1>
+        <p className="text-nowrap text-xs">Painel do Administrador</p>
+        <Separator className="mt-1" />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link to={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}

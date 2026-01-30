da38f2858206c3597be7ee0b51957e238dfc2b68
André Gabriel <andreon098@gmail.com>
2026-01-30 16:31:52 -0300

    feat: fix stale time

--- src/main.tsx
+++ src/main.tsx
@@ -10,10 +10,7 @@ import './index.css';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: false,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      refetchOnMount: false,
+      staleTime: 1000 * 60 * 5,
     },
   },
 });

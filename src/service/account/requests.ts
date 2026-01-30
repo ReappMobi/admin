7779cb06668494256a711b6ade0dc9efe4e0897d
André Gabriel <andreon098@gmail.com>
2026-01-30 16:09:51 -0300

    feat: add useGetDonorsAccount

--- src/service/account/requests.ts
+++ src/service/account/requests.ts
@@ -1,37 +1,51 @@
+import { useAuthStore } from '@/store/auth.store';
 import type { AccountStatus } from '@/types/account';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import {
+  getDonorsAccounts,
   getInstitutionsAccounts,
   updateAccountStatus,
 } from './account.service';
 
 type UseGetInstitutionsParams = {
-  token: string | null;
+  status?: AccountStatus;
+};
+
+type UseGetDonorsParams = {
   status?: AccountStatus;
 };
 
 type UseUpdateAccountStatusParams = {
-  token: string | null;
   accountId: number;
   status: AccountStatus;
 };
 
-export const useGetInstitutionsAccounts = ({
-  token,
-  ...params
-}: UseGetInstitutionsParams) =>
-  useQuery({
+export const useGetInstitutionsAccounts = (
+  params: UseGetInstitutionsParams,
+) => {
+  const token = useAuthStore((state) => state.token);
+  return useQuery({
     queryKey: ['institutions', { params }],
-    queryFn: () => getInstitutionsAccounts({ token, ...params }),
+    queryFn: () => getInstitutionsAccounts(params),
     enabled: !!token,
   });
+};
+
+export const useGetDonorsAccounts = (params?: UseGetDonorsParams) => {
+  const token = useAuthStore((state) => state.token);
+  return useQuery({
+    queryKey: ['donors', { params }],
+    queryFn: () => getDonorsAccounts(params),
+    enabled: !!token,
+  });
+};
 
 export const useUpdateAccountStatus = (accountId: number) => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationKey: ['accountStatus', accountId],
-    mutationFn: ({ token, accountId, status }: UseUpdateAccountStatusParams) =>
-      updateAccountStatus({ token, accountId, status }),
+    mutationFn: ({ accountId, status }: UseUpdateAccountStatusParams) =>
+      updateAccountStatus({ accountId, status }),
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['institutions'],
